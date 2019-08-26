011d08d88ccf14916e6cae258952c87755912c6a
Arien Kock <arien@positor.nl>
2019-08-26 13:24:33 +0200

    Add Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+// src/components/Title.tsx
+import * as React from 'react';
+
+class Title extends React.Component {
+  public render() {
+    return <h1>Test</h1>;
+  }
+}
+
+export default Title;
